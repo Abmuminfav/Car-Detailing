e18f2060ff7834999f53353e0c81fac9273c1862
Abdulmumin <abdulmumin@mailbox.unideb.hu>
2026-02-06 00:40:53 -1200

    service part

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,7 @@ import corv from "../assets/corvete.jpg"
 function Content(){
     return(
         <div className="bg-black">
-            <div className="max-w-[1280px] mx-auto flex flex-col md:flex-row items-center p-4 gap-10">
+            <div className="h-screen max-w-[1280px] mx-auto flex flex-col md:flex-row items-center p-4 gap-10">
                 <div className="md:w-1/2">
                     <h1 className="text-center text-3xl mb-4 md:text-start">About Us</h1>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab porro quae, illum harum inventore nobis? Velit at accusamus nesciunt inventore, eligendi, fugiat amet dolor labore earum sit unde exercitationem facilis?</p>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,8 +1,34 @@
+import prosche from "../assets/porsche.jpg"
+import enzo from "../assets/enzo.jpg"
+import mazda from "../assets/mazda.jpg"
 function Services(){
     return(
-        <div className="">
+       <div className="bg-[#161a1d]">
+  <div className="min-h-screen max-w-[1280px] mx-auto flex flex-col justify-center items-center gap-10 px-4">
+    
+    <h1 className="text-center text-5xl">Popular Services</h1>
 
-        </div>
+    <div className="flex flex-col md:flex-row gap-6 text-center items-center">
+      <div className="flex flex-col gap-2">
+        <img src={enzo} className="h-[300px] w-[300px] object-cover rounded-md" />
+        <h1>Full Interior Detailing</h1>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <img src={prosche} className="h-[300px] w-[300px] object-cover rounded-md" />
+        <h1>Ceramic Coating</h1>
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <img src={mazda} className="h-[300px] w-[300px] object-cover rounded-md" />
+        <h1>Premium Wash</h1>
+      </div>
+    </div>
+
+    <button className="border p-3 rounded">View all Services</button>
+  </div>
+</div>
+
     )
 };
 
